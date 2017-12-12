d9b9bf678948fc8a20ec6dca1edef8184193055d
amalsalah <33463853+amalsalah@users.noreply.github.com>
2017-12-12 01:50:34 +0100

    Add files via upload

---
+++ Services/ClaimService.cs
@@ -0,0 +1,22 @@
+﻿using Data.Infrastructure;
+using Domaine.Entity;
+using Service.Pattern;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service
+{
+  public  class ClaimService : Service<claim>, IClaimService
+    {
+        private static IDatabaseFactory dbf = new DatabaseFactory();
+        private static IUnitOfWork utk = new UnitOfWork(dbf);
+
+
+        public ClaimService() : base(utk)
+        {
+        }
+    }
+}
