58a3595bd6941acc47286d463ac2199e6afed9ae
amalsalah <33463853+amalsalah@users.noreply.github.com>
2017-12-12 01:54:16 +0100

    Add files via upload

---
+++ CGA-WEB/Models/ClaimModel.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace CGA.Models
+{
+    public class ClaimModel
+    {
+        public int id { get; set; }
+        [DataType(DataType.Date)]
+
+       
+  public DateTime? accidentDate { get; set; }
+
+        [Required(ErrorMessage = "Name: You must enter a value")]
+       
+        public string localisation { get; set; }
+
+        [Required(ErrorMessage = "Name: You must enter a value")]
+        
+        public string namesAddressesPhonesWitnesses { get; set; }
+      //  [DataType(DataType.ImageUrl), Display(Name = "Image")]
+        public string ImageUrl { get; set; }
+        [Required(ErrorMessage = "Name: You must enter a value")]
+
+      
+        public string cinInsured2 { get; set; }
+        [Required(ErrorMessage = "Name: You must enter a value")]
+
+      
+        public string lang { get; set; }
+        [Required(ErrorMessage = "Name: You must enter a value")]
+
+        
+        public string lat { get; set; }
+        [Required(ErrorMessage = "Name: You must enter a value")]
+
+       
+        public string etat { get; set; }
+        public int? contract_id { get; set; }
+
+        public int injured { get; set; }
+        [Required(ErrorMessage = "Name: You must enter a value")]
+
+       
+        public string otherObjectDamage { get; set; }
+        [Required(ErrorMessage = "Name: You must enter a value")]
+        public int vehiclesDamage { get; set; }
+    }
+}
---
+++ CGA-WEB/Models/ContractModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CGA.Models
+{
+    public class ContractModel
+    {
+        public int id { get; set; }
+
+        public float costContract { get; set; }
+
+        public DateTime? dateSending { get; set; }
+
+        public DateTime? endDate { get; set; }
+
+        public string etat { get; set; }
+
+        public DateTime? startDate { get; set; }
+
+        public int? insured_id { get; set; }
+
+        public int? policy_id { get; set; }
+    }
+}
