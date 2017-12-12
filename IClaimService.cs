db6adac8fa92e2ef6f02474591b3e157339960f5
amalsalah <33463853+amalsalah@users.noreply.github.com>
2017-12-12 01:51:14 +0100

    Add files via upload

---
+++ IClaimService.cs
@@ -0,0 +1,14 @@
+﻿using Domaine.Entity;
+using Service.Pattern;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service
+{
+ public   interface IClaimService : IService<claim>
+    {
+    }
+}
